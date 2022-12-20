8f6cbfb51c86bb06b9a87c8ceb761f0893741ba7
Romuru <pastelblue0721@gmail.com>
2022-12-21 00:27:30 +0900

    feat(login) compare to DB

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,20 @@
 import apiFetchHanlder from "@libs/server/apiFetchHandler";
+import client from "@libs/server/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).end();
+  const { phone, email } = req.body;
+  const loginTryType = phone ? { phone: +phone } : { email };
+
+  const user = await client.user.upsert({
+    where: {
+      ...loginTryType,
+    },
+    create: { name: "gest", ...loginTryType },
+    update: {},
+  });
+  console.log(user);
+  return res.status(200).end();
 }
 
 export default apiFetchHanlder("POST", handler);
