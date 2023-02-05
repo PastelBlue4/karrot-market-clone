be344395e47508d64da0fb43ba67e133c8d885f7
Romuru <pastelblue0721@gmail.com>
2023-02-05 23:59:45 +0900

    feat. swr fetch, replace enter page

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,5 +1,17 @@
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("api/users/me", fetcher);
+  const router = useRouter();
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+
+  return { user: data?.profile, isLoading: !data && !error };
 }
