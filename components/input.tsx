5afdde1ae9b8b3d62b177e2a61aa036275a87059
Romuru <pastelblue0721@gmail.com>
2023-02-06 14:30:27 +0900

    feat products prisma schema, upload page input

--- components/input.tsx
+++ components/input.tsx
@@ -6,7 +6,7 @@ interface InputProps {
   label: string;
   name: string;
   kind?: "text" | "phone" | "price";
-  register?: UseFormRegisterReturn;
+  register: UseFormRegisterReturn;
   type: string;
   required: boolean;
 }
@@ -22,7 +22,7 @@ export default function Input({
   return (
     <div>
       <label
-        className="block mb-1 text-sm font-medium text-gray-700"
+        className="block my-2 text-lg font-medium text-gray-700 "
         htmlFor={name}
       >
         {label}
@@ -41,17 +41,18 @@ export default function Input({
       {kind === "price" ? (
         <div className="relative flex items-center rounded-md shadow-sm">
           <div className="absolute left-0 flex items-center justify-center pl-3 pointer-events-none">
-            <span className="text-sm text-gray-500">$</span>
+            <span className="text-sm text-gray-500">₩</span>
           </div>
           <input
             id={name}
             type={type}
+            placeholder=""
             {...register}
             required={required}
             className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none pl-7 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
           <div className="absolute right-0 flex items-center pr-3 pointer-events-none">
-            <span className="text-gray-500">KRW</span>
+            <span className="text-gray-500"></span>
           </div>
         </div>
       ) : null}
--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,23 +1,32 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
 interface TextAreaProps {
   label?: string;
   name?: string;
+  register: UseFormRegisterReturn;
   [key: string]: any;
 }
 
-export default function TextArea({ label, name, ...rest }: TextAreaProps) {
+export default function TextArea({
+  label,
+  name,
+  register,
+  ...rest
+}: TextAreaProps) {
   return (
     <div>
       {label ? (
         <label
           htmlFor={name}
-          className="block mb-1 text-sm font-medium text-gray-700"
+          className="block my-2 text-lg font-medium text-gray-700"
         >
           {label}
         </label>
       ) : null}
       <textarea
         id={name}
-        className="w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500 "
+        {...register}
+        className="w-full h-56 my-2 border-gray-300 rounded-md shadow-sm resize-none focus:ring-orange-500 focus:border-orange-500"
         rows={4}
         {...rest}
       />
