3244139b840686099fdb1c240f9ce54380cd95a5
Romuru <pastelblue0721@gmail.com>
2023-02-06 11:11:42 +0900

    feat global swr logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="flex justify-center ">
       <div className="min-w-[568px] max-w-xl h-auto">
-        <Component {...pageProps} />
+        <SWRConfig
+          value={{
+            fetcher: (url: string) =>
+              fetch(url).then((response) => response.json()),
+          }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
       </div>{" "}
     </div>
   );
