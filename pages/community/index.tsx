0c020e64a8e660158c7040ffc254dffb0832526d
Romuru <pastelblue0721@gmail.com>
2023-03-09 02:11:42 +0900

    feat. questions floating button add

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Layout from "@components/layout";
+import FloatingButton from "@components/floating-button";
 
 const Community: NextPage = () => {
   return (
@@ -68,6 +69,23 @@ const Community: NextPage = () => {
           </svg>
         </button>
       </div>
+      <FloatingButton href="/community/write">
+        <svg
+          className="w-6 h-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </FloatingButton>
     </Layout>
   );
 };
