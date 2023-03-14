beaeecff6cb8063581285d4e4e6c2c8cb1c3d78e
Romuru <pastelblue0721@gmail.com>
2023-03-14 17:47:44 +0900

    feat.  getTimeAgo moved to utils.ts add getLocalDateString fn in  utils.ts

--- libs/client/utils.ts
+++ libs/client/utils.ts
@@ -1,3 +1,25 @@
 export function classNameHandler(...classnames: string[]) {
   return classnames.join(" ");
 }
+
+export const getTimeAgo = (time: Date) => {
+  const now = new Date();
+  const updatedAt = new Date(time);
+  const elapsed = now.getTime() - updatedAt.getTime();
+
+  const seconds = Math.floor(elapsed / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) return `${days}일 전`;
+  if (hours > 0) return `${hours}시간 전`;
+  if (minutes > 0) return `${minutes}분 전`;
+  if (seconds > 0) return `${seconds}초 전`;
+
+  return "방금 전";
+};
+
+export const getLocalDateString = (time: Date) => {
+  return new Date(time).toLocaleDateString("ko-KR");
+};
--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -26,7 +26,7 @@ export default function withHandler({
       return res.status(405).end();
     }
     if (isPrivate && !req.session.user) {
-      return res.status(401).json({ ok: false, error: "Plz log in." });
+      return res.status(401).json({ ok: false, error: "비로그인" });
     }
     try {
       await handler(req, res);
