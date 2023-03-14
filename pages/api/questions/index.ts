942fbb1b98c05bb414c82ad526fac416c4c0f233
Romuru <pastelblue0721@gmail.com>
2023-03-14 17:46:29 +0900

    feat. questions fetch data (before UI)

--- pages/api/questions/index.ts
+++ pages/api/questions/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
+import answers from "./[id]/answers";
 
 async function handler(
   req: NextApiRequest,
@@ -12,26 +13,53 @@ async function handler(
     session: { user },
   } = req;
 
-  const question = await client.question.create({
-    data: {
-      contents,
-      user: {
-        connect: {
-          id: user?.id,
+  if (req.method === "GET") {
+    const getQuestions = await client.question.findMany({
+      include: {
+        user: {
+          select: {
+            name: true,
+            avatar: true,
+            id: true,
+          },
+        },
+        _count: {
+          select: {
+            interests: true,
+            answers: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  res.json({
-    ok: true,
-    question,
-  });
+    res.json({
+      ok: true,
+      questions: getQuestions,
+    });
+  }
+
+  if (req.method === "POST") {
+    const question = await client.question.create({
+      data: {
+        contents,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+
+    res.json({
+      ok: true,
+      question,
+    });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["POST"],
+    methods: ["GET", "POST"],
     handler,
   })
 );
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,72 +1,81 @@
 import type { NextPage } from "next";
 import Layout from "@components/layout";
 import FloatingButton from "@components/floating-button";
+import useSWR from "swr";
+import Link from "next/link";
+import { Question, User } from "@prisma/client";
+import { getLocalDateString, getTimeAgo } from "@libs/client/utils";
+
+interface QuestionsWithUser extends Question {
+  user: User;
+  _count: {
+    interests: number;
+    answers: number;
+  };
+}
+
+interface QuestionsResponse {
+  ok: boolean;
+  questions: QuestionsWithUser[];
+}
 
 const Community: NextPage = () => {
+  const { data } = useSWR<QuestionsResponse>(`api/questions`);
+
   return (
     <Layout hasTabBar title="동네생활">
       <div>
-        <div>
-          <span>동네질문</span>
-          <span>
-            <span>Q.</span> What is the best mandu restaurant?
-          </span>
-          <div>
-            <span>니꼬</span>
-            <span>18시간 전</span>
-          </div>
-          <div>
-            <span>
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
-              <span>궁금해요 1</span>
-            </span>
-            <span>
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              <span>답변 1</span>
-            </span>
-          </div>
-        </div>
-        <button>
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-            ></path>
-          </svg>
-        </button>
+        {data &&
+          data?.questions.map((question) => (
+            <Link key={question.id} href={`/community/${question.id}`}>
+              <a className="flex flex-col items-start pt-4 cursor-pointer">
+                <div className="px-4 mt-2 text-gray-700">
+                  <span className="font-medium text-orange-500">Q.</span>{" "}
+                  {question.contents}
+                </div>
+                <div className="flex items-center justify-between w-full px-4 mt-5 text-xs font-medium text-gray-500">
+                  <span>{question.user.name}</span>
+                  <span>{getTimeAgo(question.updatedAt)}</span>
+                </div>
+                <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t   w-full">
+                  <span className="flex items-center space-x-2 text-sm">
+                    <svg
+                      className="w-5 h-5"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      ></path>
+                    </svg>
+                    <span>관심 {question._count.interests}</span>
+                  </span>
+                  <span className="flex items-center space-x-2 text-sm">
+                    <svg
+                      className="w-5 h-5"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                      ></path>
+                    </svg>
+                    <span>답변 {question._count.answers}</span>
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
       </div>
       <FloatingButton href="/community/write">
         <svg
